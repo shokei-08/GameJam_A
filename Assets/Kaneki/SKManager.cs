e5c8cfc47821db881c5c83365c9252bcc6440d4e
kanekivantan0813 <kanekivantan0813@gmail.com>
2026-06-23 14:41:29 +0900

    a

--- Assets/Kaneki/SKManager.cs
+++ Assets/Kaneki/SKManager.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class KekkaManager : MonoBehaviour
+public class SKManager : MonoBehaviour
 {
+    public Text NameText;
+    public Text NaiyouText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
