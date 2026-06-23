117b38a9614b029affd4f9f5222a281be8f324f5
kritsuto11-star <kritsuto11@gmail.com>
2026-06-23 13:38:48 +0900

    henkou

---
+++ Assets/Ritsuto/main story.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public Image Image; //画像を入れるオブジェクト
+    public Sprite title;//タイトル画像
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
