b875a003c160dba17cb5aa7915882c50c528d792
kritsuto11-star <kritsuto11@gmail.com>
2026-06-23 14:21:24 +0900

    a

--- Assets/Ritsuto/main story.cs
+++ Assets/Ritsuto/main story.cs
@@ -4,6 +4,13 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     public Image Image; //画像を入れるオブジェクト
     public Sprite title;//タイトル画像
+    public GameObject selans;
+    public Text Q;
+    public Text sel1;
+    public Text sel2;
+    public Text sel3;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
